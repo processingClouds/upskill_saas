e46fadded7557397e7391f6dc851654fe5738106
S A <processingclouds@gmail.com>
2024-04-24 09:39:44 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_225055) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_092817) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_225055) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
